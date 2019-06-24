b4c627b1c598323907c0e8523d4acd38fcd0aead
Roshan Pandey <pandeyroshan556@gmail.com>
2019-06-24 20:02:28 +0530

    Update admin.py

--- hostel/admin.py
+++ hostel/admin.py
@@ -4,6 +4,8 @@ from .models import AdminAddress
 from .models import MessageData
 # Register your models here.
 
+admin.site.site_title = 'NUH'
+admin.site.site_header = 'NUH ADMIN PANEL'
 admin.site.register(RoomType)
 admin.site.register(AdminAddress)
 admin.site.register(MessageData)
